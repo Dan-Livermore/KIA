5b743732c54e88abf0cf21070092530a459183e0
Dan Livermore <danieljlivermore@gmail.com>
2026-09-04 15:13:36 +0100

    table head and dynamically filled from hard coded data

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const form = document.getElementById("form-kia");
 const submitButton = document.getElementById("submitButton");
 const statusMessage = document.getElementById("status");
 
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx8Pm_taFzeY5wdWw2_SV1ouh0tFlSXS1GXVeSEbDXLgQiQPElmMglsFOCTPIK8B5A/exec";
+const APPS_SCRIPT_URL =
+  "https://script.google.com/macros/s/AKfycbx8Pm_taFzeY5wdWw2_SV1ouh0tFlSXS1GXVeSEbDXLgQiQPElmMglsFOCTPIK8B5A/exec";
 
 form.addEventListener("submit", async function (event) {
   event.preventDefault();
@@ -45,3 +46,67 @@ form.addEventListener("submit", async function (event) {
   submitButton.disabled = false;
   submitButton.textContent = "Submit";
 });
+
+const data = [
+  {
+    car: "Fast Car",
+    customer: "Mr Smith",
+    startTime: "2026-09-03 09:14",
+    endTime: "2026-09-03 12:36",
+  },
+  {
+    car: "Blue Car",
+    customer: "Mrs Jones",
+    startTime: "2026-09-03 13:14",
+    endTime: "2026-09-03 13:48",
+  },
+  {
+    car: "Red Car",
+    customer: "John Doe",
+    startTime: "2026-09-04 14:00",
+    endTime: "2026-09-04 15:00",
+  },
+];
+
+buildTable(data);
+
+function buildTable(data) {
+  if (!data.length) return;
+
+  const table = document.getElementById("table");
+  const thead = table.querySelector("thead");
+  const tbody = table.querySelector("tbody");
+
+  thead.innerHTML = "";
+  tbody.innerHTML = "";
+
+  const headers = Object.keys(data[0]);
+
+const headerRow = document.createElement("tr");
+
+headerRow.innerHTML = `
+                      <th>Car Name</th>
+                      <th>Customer Name</th>
+                      <th>Start Time</th>
+                      <th>End Time</th>
+                      <th>Delete</th>
+                      `;
+
+thead.appendChild(headerRow);
+
+  data.forEach((item) => {
+    const row = document.createElement("tr");
+
+    headers.forEach((header) => {
+      const td = document.createElement("td");
+      if (header === "startTime" || header === "endTime") {
+        td.textContent = new Date(item[header]).toLocaleString('en-GB', { day: '2-digit', month: 'short', year: 'numeric', hour: '2-digit', minute: '2-digit'});
+      } else {
+        td.textContent = item[header];
+      }
+      row.appendChild(td);
+    });
+
+    tbody.appendChild(row);
+  });
+}
